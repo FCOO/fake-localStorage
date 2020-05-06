d97db7b87f8cc11c7dedeef0ad019ef9c2006546
Niels Holt <niels@steenbuchholt.dk>
2020-05-06 17:52:01 +0200

    Adjust localStorage-test

--- src/fake-localstorage.js
+++ src/fake-localstorage.js
@@ -1,5 +1,5 @@
 /****************************************************************************
-	fake-localstorage.js, 
+	fake-localstorage.js,
 
 	(c) 2017, FCOO
 
@@ -10,54 +10,50 @@
 
 (function (window/*, document, undefined*/) {
 	"use strict";
-	
+
     /*********************************************************************
     Determinate if localStorage is supported and available
     If the browser is in 'Private' mode not all browser supports localStorage
     In localStorage isn't supported a fake version is installed
     At the moment no warning is given when localStorage isn't supported since
-    some browser in private-mode allows the use of window.localStorage but 
+    some browser in private-mode allows the use of window.localStorage but
     don't save it when the session ends
     *********************************************************************/
     window.fake_localstorage_installed = false;
 
-    // Test taken from https://gist.github.com/engelfrost/fd707819658f72b42f55
-    if (typeof window.localStorage === 'object') {
-        // Safari will throw a fit if we try to use localStorage.setItem in private browsing mode. 
-        try {
-            localStorage.setItem('localStorageTest', 1);
-            localStorage.removeItem('localStorageTest');
-            window.fake_localstorage_installed = false;
-        } 
-        catch (e) {
-            window.fake_localstorage_installed = true;
-        }
-    } 
-    else 
-        window.fake_localstorage_installed = true;        
+    // Test taken from https://gist.github.com/engelfrost/fd707819658f72b42f55 and https://dev.to/zigabrencic/web-browser-local-storage-16jh
+
+    // Safari will throw a fit if we try to use localStorage.setItem in private browsing mode.
+    try {
+        localStorage.setItem('localStorageTest', 1);
+        localStorage.removeItem('localStorageTest');
+    }
+    catch (e) {
+        window.fake_localstorage_installed = true;
+    }
 
     if (window.fake_localstorage_installed){
         /*********************************************************************
         Create a fake localStorage for any browser that does not support it.
 
         Taken from https://gist.github.com/engelfrost/fd707819658f72b42f55:
-            Fake localStorage implementation. 
-            Mimics localStorage, including events. 
-            It will work just like localStorage, except for the persistant storage part. 
+            Fake localStorage implementation.
+            Mimics localStorage, including events.
+            It will work just like localStorage, except for the persistant storage part.
         *********************************************************************/
         var fakeLocalStorage = {};
-        var storage; 
-  
-        // If Storage exists we modify it to write to our fakeLocalStorage object instead. 
-        // If Storage does not exist we create an empty object. 
+        var storage;
+
+        // If Storage exists we modify it to write to our fakeLocalStorage object instead.
+        // If Storage does not exist we create an empty object.
         if (window.Storage && window.localStorage) {
-            storage = window.Storage.prototype; 
+            storage = window.Storage.prototype;
         } else {
             // We don't bother implementing a fake Storage object
-            window.localStorage = {}; 
-            storage = window.localStorage; 
+            window.localStorage = {};
+            storage = window.localStorage;
         }
-  
+
         // For older IE
         if (!window.location.origin) {
             window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
